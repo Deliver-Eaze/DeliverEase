ee871a2784513b49bef8475b35b16d7f9023749b
zeinab omer <zomer2580@gmail.com>
2026-07-25 16:18:58 +0200

    version3

--- DeliverEase/admin.py
+++ DeliverEase/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
+from .models import User , Category , MenuItem , Order , OrderItem, Payment
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+   list_display = ('username', 'email','role' , 'is_staff')
+   fieldsets = BaseUserAdmin.fieldsets +(('Role',{'fields':('role',)}),
+   )
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Category)
+admin.site.register(MenuItem )
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+
